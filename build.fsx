1c43d1a33d22618bf508eb83ddeec4ace5769fd2
0918nobita <nobita.0918@gmail.com>
2021-08-18 23:21:26 +0900

    :sparkles: index.html を生成するようにした

--- build.fsx
+++ build.fsx
@@ -2,10 +2,11 @@
 #r "nuget: Markdig, 0.25.0"
 
 open System.IO
+open System.Text.RegularExpressions
 open FsToolkit.ErrorHandling
 open Markdig
 
-let compileAsync ((inputPath, outputPath) : string * string) : Async<Result<unit, string>> =
+let compileAsync (inputPath: string) (outputPath: string) : Async<Result<unit, string>> =
     asyncResult {
         let text = File.ReadAllText inputPath
 
@@ -50,8 +51,53 @@ let sumResultSeq (resSeq: seq<Result<'a, 'b>>) : Result<seq<'a>, seq<'b>> =
 
 Directory.CreateDirectory "build"
 
+let tryGetPageNumber (filename : string) =
+    let m = Regex("^([0-9]+)-").Match filename
+    if m.Groups.Count = 2
+    then
+        m.Groups.[1].Value
+        |> int
+        |> Some
+    else
+        None
+
+let mdFiles = Directory.EnumerateFiles "pages"
+
+let genIndexHtmlAsync =
+    asyncResult {
+        let htmlFiles =
+            mdFiles
+            |> Seq.map
+                (Path.GetFileNameWithoutExtension
+                >> sprintf "%s.html")
+        let liTags =
+            htmlFiles
+            |> Seq.sortWith (fun a b ->
+                option {
+                    let! aPageNum = tryGetPageNumber a
+                    let! bPageNum = tryGetPageNumber b
+                    return aPageNum - bPageNum
+                }
+                |> Option.defaultValue 0)
+            |> Seq.map (fun file -> (sprintf """<li><a href="%s">%s</a></li>""") file file)
+            |> String.concat ""
+        let html = $"""<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>プログラミングをはじめよう</title>
+</head>
+<body>
+<h1>プログラミングをはじめよう</h1>
+<ul>{liTags}</ul>
+</body>
+</html>
+"""
+        File.WriteAllText ("build/index.html", html)
+    }
+
 let res =
-    Directory.EnumerateFiles "pages"
+    mdFiles
     |> Seq.map (
         (fun inputPath ->
             let outputPath =
@@ -60,7 +106,8 @@ let res =
                 |> sprintf "%s.html"
                 |> fun filename -> Path.Combine ("build", filename)
             (inputPath, outputPath))
-        >> compileAsync)
+        >> (fun (inputPath, outputPath) -> compileAsync inputPath outputPath))
+    |> Seq.append (Seq.singleton genIndexHtmlAsync)
     |> Async.Parallel
     |> Async.RunSynchronously
     |> sumResultSeq
