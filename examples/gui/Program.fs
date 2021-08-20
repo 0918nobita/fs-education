60be6fa40d5a3749fa35b2f96dc4c3a1da4f330c
0918nobita <nobita.0918@gmail.com>
2021-08-20 19:25:12 +0900

    GTK アプリケーションのサンプルを追加した

---
+++ examples/gui/Program.fs
@@ -0,0 +1,19 @@
+﻿namespace gui
+
+module Program =
+    open Gtk
+
+    [<EntryPoint>]
+    let main argv =
+        Application.Init()
+
+        let app = new Application("org.gui.gui", GLib.ApplicationFlags.None)
+        app.Register(GLib.Cancellable.Current) |> ignore;
+
+        let win = new MainWindow()
+        app.AddWindow(win)
+
+        win.Show()
+
+        Application.Run()
+        0
---
+++ pages/10-gui-app.md
@@ -0,0 +1,8 @@
+# GUI アプリケーションを作ろう
+
+いよいよ今回からはウィンドウの表示される本格的なプログラムを作っていきます
+
+```bash
+dotnet new -i GtkSharp.Template.FSharp::3.24.24.34
+dotnet new gtkapp -lang="F#" -o gui
+```
