03499109a40e260656b38d246f4581076e894409
0918nobita <nobita.0918@gmail.com>
2021-09-04 12:44:59 +0900

    :sparkles: アイコンを追加した

--- Program.fs
+++ Program.fs
@@ -56,7 +56,10 @@ let commonTags =
       link [ _rel "stylesheet"
              _href "style.css" ]
       link [ _rel "manifest"
-             _href "manifest.json" ] ]
+             _href "manifest.json" ]
+      link [ _rel "icon"
+             _href
+                 "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🔷</text></svg>" ] ]
 
 let genSinglePageInfoAsync (markdownPath: string) =
     asyncResult {
